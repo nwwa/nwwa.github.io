2ee425776e63d3ec7d110d226465bbd18dd756fd
Tony Grosinger <tony@grosinger.net>
2017-11-24 21:00:17 -0800

    WIP: Stripe portion is basically done

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 	"os"
 
 	stripe "github.com/stripe/stripe-go"
-	"github.com/stripe/stripe-go/charge"
 	"github.com/stripe/stripe-go/customer"
+	"github.com/stripe/stripe-go/sub"
 )
 
 var (
@@ -17,9 +17,25 @@ var (
 	publishableKey = os.Getenv("PUBLISHABLE_KEY")
 )
 
+func logError(r *http.Request, err error, msg string) {
+	// TODO: Do something with the request to give more context
+	// Such as printing the email, phone, and name so I can contact them
+	fmt.Printf("%s: %s\n", msg, err)
+}
+
+func renderSignupSuccess(w http.ResponseWriter) {
+	tmpl, _ := template.Must(masterTmpl.Clone()).ParseFiles("templates/signup-success.html")
+	tmpl.Execute(w, map[string]string{"title": "Membership Created"})
+}
+
+func renderSignupFailure(w http.ResponseWriter) {
+	tmpl, _ := template.Must(masterTmpl.Clone()).ParseFiles("templates/signup-failure.html")
+	tmpl.Execute(w, map[string]string{"title": "Membership Failed"})
+}
+
 func handleSignup(w http.ResponseWriter, r *http.Request) {
-	tmpl, _ := template.Must(masterTmpl.Clone()).ParseFiles("signup.html")
-	tmpl.Execute(w, map[string]string{"Key": publishableKey, "title": "testing"})
+	tmpl, _ := template.Must(masterTmpl.Clone()).ParseFiles("templates/signup.html")
+	tmpl.Execute(w, map[string]string{"Key": publishableKey, "title": "Membership Signup"})
 }
 
 func handleCharge(w http.ResponseWriter, r *http.Request) {
@@ -29,31 +45,33 @@ func handleCharge(w http.ResponseWriter, r *http.Request) {
 	customerParams.SetSource(r.Form.Get("stripeToken"))
 
 	newCustomer, err := customer.New(customerParams)
-
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		logError(r, err, "Failed to create a customer during signup")
+		renderSignupFailure(w)
 		return
 	}
 
-	chargeParams := &stripe.ChargeParams{
-		Amount:   500,
-		Currency: "usd",
-		Desc:     "Sample Charge",
+	subParams := stripe.SubParams{
 		Customer: newCustomer.ID,
+		Items: []*stripe.SubItemsParams{
+			{
+				Plan: "membership",
+			},
+		},
 	}
-
-	if _, err := charge.New(chargeParams); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if _, err := sub.New(&subParams); err != nil {
+		logError(r, err, "Failed to create a subscription during signup")
+		renderSignupFailure(w)
 		return
 	}
 
-	fmt.Fprintf(w, "Charge completed successfully!")
+	renderSignupSuccess(w)
 }
 
 func main() {
 	stripe.Key = os.Getenv("SECRET_KEY")
 
-	masterTmpl, _ = template.ParseFiles("master.html")
+	masterTmpl, _ = template.ParseFiles("templates/master.html")
 	http.HandleFunc("/signup", handleSignup)
 	http.HandleFunc("/charge", handleCharge)
 
